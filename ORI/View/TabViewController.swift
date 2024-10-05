cb82a3c2b3f3d3516c7373e970da83ee617ab948
song <nadana0929@gmail.com>
2024-10-05 21:05:42 +0900

    Feat: 탭바 완성

--- ORI/View/TabViewController.swift
+++ ORI/View/TabViewController.swift
@@ -8,22 +8,45 @@
 import UIKit
 
 class TabViewController: UIViewController {
+    
+    let tabBarVC = UITabBarController()
+    
+    let vc1 = UINavigationController(rootViewController: FirstViewController())
+    let vc2 = SecondViewController()
+    let vc3 = ThirdViewController()
+    let vc4 = SecondViewController()
+    let vc5 = ThirdViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let customTabBar = CustomTabBar()
+         tabBarVC.setValue(customTabBar, forKey: "tabBar")
+        
+        tabBarVC.setViewControllers([vc1, vc2, vc3, vc4, vc5], animated: false)
+        tabBarVC.modalPresentationStyle = .fullScreen
+        tabBarVC.tabBar.backgroundColor = .white
+        
+        guard let items = tabBarVC.tabBar.items else { return }
+        items[0].image = UIImage(systemName: "line.horizontal.3")
+        items[1].image = UIImage(systemName: "chevron.left.forwardslash.chevron.right")
+        items[2].image = UIImage(systemName: "square.and.pencil")
+        items[3].image = UIImage(systemName: "bell")
+        items[4].image = UIImage(systemName: "person")
+        
+        addChild(tabBarVC)
+        view.addSubview(tabBarVC.view)
+        tabBarVC.view.frame = view.bounds
+        tabBarVC.didMove(toParent: self)
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
+import UIKit
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+class CustomTabBar: UITabBar {
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        var size = super.sizeThatFits(size)
+        size.height = 100 // 원하는 높이 설정
+        return size
     }
-    */
-
 }
