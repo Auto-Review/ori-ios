babcf53093a30be5505531b80863b63cd23945bb
song <nadana0929@gmail.com>
2024-10-14 20:54:44 +0900

    Chore: 프로젝트 생성

---
+++ ORI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ORI
+//
+//  Created by Song Kim on 10/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
