edc25391a1150c66ac6619ea41696795cbb1c31b
song <nadana0929@gmail.com>
2024-10-24 20:53:53 +0900

    Fix: Data Model 수정

--- ORI/Model/Post.swift
+++ ORI/Model/Post.swift
@@ -11,7 +11,6 @@ struct TILPost {
     var title: String
     var member_id: Int64
     var timer: String
-    var code: String
     var description: String
     var images: [Int64: String]
     var createdTime: Date
