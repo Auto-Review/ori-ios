e99f128351ff24aff2696dcbcb9dd5905bccd448
song <nadana0929@gmail.com>
2024-10-24 20:49:52 +0900

    Feat: Post에 CodePost 모델 추가

--- ORI/Model/Post.swift
+++ ORI/Model/Post.swift
@@ -17,3 +17,15 @@ struct TILPost {
     var createdTime: Date
     var reviewTime: String
 }
+
+struct CodePost {
+    var title: String
+    var member_id: Int64
+    var level: Double
+    var timer: String
+    var code: String
+    var description: String
+    var images: [Int64: String]
+    var createdTime: Date
+    var reviewTime: String
+}
