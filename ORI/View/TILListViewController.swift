5fdd4af398464e4fc1641d285b5bb83cfd429d22
song <nadana0929@gmail.com>
2024-10-29 14:08:21 +0900

    Design: 디자인 변경

--- ORI/View/TILListViewController.swift
+++ ORI/View/TILListViewController.swift
@@ -7,39 +7,6 @@
 
 import UIKit
 
-class CustomTILCell: UITableViewCell {
-    let titleLabel = UILabel()
-    let createdTimeLabel = UILabel()
-    let descriptionLabel = UILabel()
-    
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        let stackView = UIStackView(arrangedSubviews: [titleLabel, createdTimeLabel, descriptionLabel])
-        stackView.axis = .vertical
-        stackView.spacing = 4
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        contentView.addSubview(stackView)
-        
-        NSLayoutConstraint.activate([
-            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-            stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
-            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
-        ])
-        
-        titleLabel.font = UIFont.boldSystemFont(ofSize: 16)
-        createdTimeLabel.font = UIFont.systemFont(ofSize: 14)
-        descriptionLabel.font = UIFont.systemFont(ofSize: 12)
-        descriptionLabel.numberOfLines = 0 // 여러 줄 표시 가능
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
-
 class TILListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var viewModel = TILListViewModel()
     var tableView: UITableView!
@@ -54,6 +21,7 @@ class TILListViewController: UIViewController, UITableViewDelegate, UITableViewD
         tableView = UITableView(frame: self.view.bounds, style: .plain)
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.separatorStyle = .none
         self.view.addSubview(tableView)
         
         tableView.register(CustomTILCell.self, forCellReuseIdentifier: "TILCell")
@@ -78,13 +46,69 @@ class TILListViewController: UIViewController, UITableViewDelegate, UITableViewD
 
         return cell
     }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 200
+
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
     }
     
-    // 셀 선택 시 행동
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(viewModel.data[indexPath.row])
+    }
+}
+
+class CustomTILCell: UITableViewCell {
+    let titleLabel = UILabel()
+    let createdTimeLabel = UILabel()
+    let descriptionLabel = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        contentView.layer.masksToBounds = false
+        contentView.layer.shadowOffset = CGSize(width: 0, height: 2)
+        contentView.layer.shadowRadius = 4
+        contentView.backgroundColor = UIColor.systemGray6
         
+        let containerView = UIView()
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.backgroundColor = .white
+        containerView.layer.cornerRadius = 10
+        contentView.addSubview(containerView)
+        
+        NSLayoutConstraint.activate([
+            containerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            containerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            containerView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            containerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
+        ])
+        
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.spacing = 8
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        containerView.addSubview(stackView)
+        
+        stackView.addArrangedSubview(titleLabel)
+        stackView.addArrangedSubview(createdTimeLabel)
+        stackView.addArrangedSubview(descriptionLabel)
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16),
+            stackView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 16),
+            stackView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -16)
+        ])
+        
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 18)
+        createdTimeLabel.font = UIFont.systemFont(ofSize: 14)
+        createdTimeLabel.textColor = UIColor.gray
+        descriptionLabel.font = UIFont.systemFont(ofSize: 16)
+        descriptionLabel.numberOfLines = 5
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
