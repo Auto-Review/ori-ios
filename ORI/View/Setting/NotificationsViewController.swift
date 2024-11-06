e850e33a2c30a31ecbc6a265f033951c9aa6852a
song <nadana0929@gmail.com>
2024-11-06 21:02:29 +0900

    Feat: 파일 정리, 필요한 뷰 컨트롤러 파일 추가

---
+++ ORI/View/Setting/NotificationsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NotificationsViewController.swift
+//  ORI
+//
+//  Created by Song Kim on 11/6/24.
+//
+
+import UIKit
+
+class NotificationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
