2fd8baf5155d930d09a6161a1edd46a8c661c9a3
song <nadana0929@gmail.com>
2024-11-06 11:26:50 +0900

    Feat: 모델과 뷰모델 생성

---
+++ ORI/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  ORI
+//
+//  Created by Song Kim on 11/6/24.
+//
+
+import Foundation
+
+struct User {
+    var name: String
+    var email: String
+    var token: String
+    var image: String
+}
---
+++ ORI/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewModel.swift
+//  ORI
+//
+//  Created by Song Kim on 11/6/24.
+//
+
+import Foundation
+
+class ProfileViewModel {
+    var user: User = User(name: "김송", email: "djfksld@fjgjklgjs.jljk", token: "kaj;lgfjjfsdl;hkjsdgkl", image: "https://i.namu.wiki/i/BR5sFz-JgZ8Tm39VPvfg99UCOBK07c8-jbB4ZVebPVSiZs_L8alLJHHAs0i07UUccO5rLmTTR1JrkjwOKTFMGRxfVNO3M1a1I2JlEy8LHHSqoOHdAE-G5I2tUji6lpPUXN8rXUWtzDNfH8m7XOraLw.webp")
+}
