428a102f00c27a985752d4cf9e851a4761239c5c
song <nadana0929@gmail.com>
2024-11-25 21:35:32 +0900

    Fix: 사용하지않는 로그인 코드 삭제

--- ORI/App/SceneDelegate.swift
+++ ORI/App/SceneDelegate.swift
@@ -11,13 +11,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-//        let mainViewController = LoginViewController()
-        let mainViewController = TabViewController()
+        let mainViewController = LoginViewController()
+//        let mainViewController = TabViewController()
 
         window?.rootViewController = mainViewController
         window?.makeKeyAndVisible()
