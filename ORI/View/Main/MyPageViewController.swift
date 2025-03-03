8be7b4b83038a259e37a7e4264e2dbbc4c19cb9d
song <nadana0929@gmail.com>
2025-03-03 18:15:30 +0900

    Feat: 수정버튼추가

--- ORI/View/Main/MyPageViewController.swift
+++ ORI/View/Main/MyPageViewController.swift
@@ -46,14 +46,51 @@ class MyPageViewController: UIViewController {
         return stackView
     }()
     
+    lazy var editButton: UIButton = {
+        let button = UIButton(type: .system)
+        let pencilIcon = UIImage(systemName: "pencil")
+        button.setImage(pencilIcon, for: .normal)
+        button.tintColor = .black
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(toggleEditMode), for: .touchUpInside)
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(myPageMainTextLabel)
         view.addSubview(grayBackgroundView)
         grayBackgroundView.addSubview(infoStackView)
+        grayBackgroundView.addSubview(editButton)
         
         mainNavigationBar()
         setupConstraints()
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            myPageMainTextLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            myPageMainTextLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 18),
+            
+            grayBackgroundView.topAnchor.constraint(equalTo: myPageMainTextLabel.bottomAnchor, constant: 14),
+            grayBackgroundView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 18),
+            grayBackgroundView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -18),
+            grayBackgroundView.heightAnchor.constraint(equalToConstant: 130),
+            
+            infoStackView.centerXAnchor.constraint(equalTo: grayBackgroundView.centerXAnchor),
+            infoStackView.centerYAnchor.constraint(equalTo: grayBackgroundView.centerYAnchor),
+            infoStackView.leadingAnchor.constraint(equalTo: grayBackgroundView.leadingAnchor, constant: 20),
+            infoStackView.trailingAnchor.constraint(equalTo: grayBackgroundView.trailingAnchor, constant: -20),
+
+            emailTextField.widthAnchor.constraint(equalToConstant: 260),
+            nameTextField.widthAnchor.constraint(equalToConstant: 260),
+            bioTextField.widthAnchor.constraint(equalToConstant: 260),
+
+            editButton.topAnchor.constraint(equalTo: grayBackgroundView.topAnchor, constant: 20),
+            editButton.trailingAnchor.constraint(equalTo: grayBackgroundView.trailingAnchor, constant: -20),
+            editButton.widthAnchor.constraint(equalToConstant: 18),
+            editButton.heightAnchor.constraint(equalToConstant: 18)
+        ])
     }
     
     func createLabel(text: String) -> UILabel {
@@ -82,24 +119,17 @@ class MyPageViewController: UIViewController {
         return stackView
     }
 
-    func setupConstraints() {
-        NSLayoutConstraint.activate([
-            myPageMainTextLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
-            myPageMainTextLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 18),
-            
-            grayBackgroundView.topAnchor.constraint(equalTo: myPageMainTextLabel.bottomAnchor, constant: 14),
-            grayBackgroundView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 18),
-            grayBackgroundView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -18),
-            grayBackgroundView.heightAnchor.constraint(equalToConstant: 130),
-            
-            infoStackView.centerXAnchor.constraint(equalTo: grayBackgroundView.centerXAnchor),
-            infoStackView.centerYAnchor.constraint(equalTo: grayBackgroundView.centerYAnchor),
-            infoStackView.leadingAnchor.constraint(equalTo: grayBackgroundView.leadingAnchor, constant: 20),
-            infoStackView.trailingAnchor.constraint(equalTo: grayBackgroundView.trailingAnchor, constant: -20),
-            
-            emailTextField.widthAnchor.constraint(equalToConstant: 260),
-            nameTextField.widthAnchor.constraint(equalToConstant: 260),
-            bioTextField.widthAnchor.constraint(equalToConstant: 260)
-        ])
+    @objc func toggleEditMode() {
+        let isEditing = emailTextField.isUserInteractionEnabled
+        emailTextField.isUserInteractionEnabled.toggle()
+        nameTextField.isUserInteractionEnabled.toggle()
+        bioTextField.isUserInteractionEnabled.toggle()
+        
+        if isEditing {
+            view.endEditing(true) // 키보드 닫기
+            editButton.setImage(UIImage(systemName: "pencil"), for: .normal)
+        } else {
+            editButton.setImage(UIImage(systemName: "tray.and.arrow.down.fill"), for: .normal)
+        }
     }
 }
