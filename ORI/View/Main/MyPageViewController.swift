b1439e4ec9e56fc1e6384e22c92bb9e0b78130f6
song <nadana0929@gmail.com>
2025-03-03 17:52:44 +0900

    Feat: 개인정보를 담는 백그라운드까지 !

--- ORI/View/Main/MyPageViewController.swift
+++ ORI/View/Main/MyPageViewController.swift
@@ -10,23 +10,37 @@ import UIKit
 class MyPageViewController: UIViewController {
     var viewModel = MyPageViewModel()
     
-    lazy var myNameTextLabel: UILabel = {
+    lazy var myPageMainTextLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "// MY PAGE"
-        label.font = UIFont.blackhansans(ofSize: 30)
+        label.font = UIFont.blackhansans(ofSize: 20)
         return label
+    }()
+    
+    lazy var grayBackgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.systemGray6
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 10
+        return view
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(myNameTextLabel)
+        view.addSubview(myPageMainTextLabel)
+        view.addSubview(grayBackgroundView)
         
         mainNavigationBar()
         
         NSLayoutConstraint.activate([
-            myNameTextLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            myNameTextLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            myPageMainTextLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 35),
+            myPageMainTextLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 18),
+            
+            grayBackgroundView.topAnchor.constraint(equalTo: myPageMainTextLabel.bottomAnchor, constant: 20),
+            grayBackgroundView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 18),
+            grayBackgroundView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -18),
+            grayBackgroundView.heightAnchor.constraint(equalToConstant: 130)
         ])
     }
 }
