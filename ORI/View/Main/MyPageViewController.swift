064f76886b35a325484b4f30064b5319d5f78100
song <nadana0929@gmail.com>
2025-03-03 17:38:11 +0900

    Feat: 제목추가

--- ORI/View/Main/MyPageViewController.swift
+++ ORI/View/Main/MyPageViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 class MyPageViewController: UIViewController {
     var viewModel = MyPageViewModel()
     
+    lazy var myNameTextLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "// MY PAGE"
+        label.font = UIFont.blackhansans(ofSize: 30)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.systemGray6
+        view.addSubview(myNameTextLabel)
         
         mainNavigationBar()
         
+        NSLayoutConstraint.activate([
+            myNameTextLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            myNameTextLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 }
