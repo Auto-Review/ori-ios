8a7bc9263ca58fd80fa1ec9ba12991c04c61c3e9
Song Kim <songkim@Songui-MacBookPro.local>
2025-03-14 17:04:50 +0900

    Fix: 높이가 모호하다고 또 경고를 보내줘서 수정

--- ORI/View/Main/MyPage/MyPostViewController.swift
+++ ORI/View/Main/MyPage/MyPostViewController.swift
@@ -61,6 +61,10 @@ class MyPostViewController: UIViewController, UITableViewDelegate, UITableViewDa
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModel.numberOfPosts()
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 40
+    }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = CustomTableViewCell()
