545bb99f3e72b54da3d04a7b60054db5bb979d12
Song Kim <songkim@Songui-MacBookPro.local>
2025-03-18 09:38:04 +0900

    Fix: 테스트한다고 넣어놓은 필요없는 코드 삭제

--- ORI/ViewModel/CodeListViewModel.swift
+++ ORI/ViewModel/CodeListViewModel.swift
@@ -12,15 +12,12 @@ class CodeListViewModel {
     var posts: [Code] = []
     var tableView = UITableView()
     
-    var didUpdateData: (() -> Void)?
-    
     func loadCodeList() {
         fetchCodeList(page: 0, size: 10) { [weak self] result in
             switch result {
             case .success(let posts):
                 self?.posts = posts
                 self?.tableView.reloadData()
-                self?.didUpdateData?()
             case .failure(let error):
                 print("Error fetching posts: \(error)")
             }
