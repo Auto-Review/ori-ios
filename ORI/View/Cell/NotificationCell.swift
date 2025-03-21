4ea489c6d36882abe4f96514a93bbd029b2a0548
Song Kim <songkim@Songui-MacBookPro.local>
2025-03-21 11:10:38 +0900

    Feat: 불러온 알림 리스트 출력

---
+++ ORI/View/Cell/NotificationCell.swift
@@ -0,0 +1,52 @@
+//
+//  NotificationCell.swift
+//  ORI
+//
+//  Created by Song Kim on 3/21/25.
+//
+
+import UIKit
+
+class NotificationCell: UITableViewCell {
+    let titleLabel = UILabel() // 날짜 + 리뷰
+    let subtitleLabel = UILabel() // 제목 (내용)
+    let iconImage = UIImageView(image: UIImage(systemName: "archivebox"))
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 19)
+        subtitleLabel.font = UIFont.systemFont(ofSize: 15)
+        subtitleLabel.textColor = .gray
+        iconImage.tintColor = .baseYellow
+        
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(subtitleLabel)
+        contentView.addSubview(iconImage)
+
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        iconImage.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            contentView.heightAnchor.constraint(greaterThanOrEqualToConstant: 100),
+            
+            iconImage.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            iconImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            iconImage.widthAnchor.constraint(equalToConstant: 40),
+            iconImage.heightAnchor.constraint(equalToConstant: 40),
+            
+            titleLabel.centerYAnchor.constraint(equalTo: iconImage.centerYAnchor, constant: -9),
+            titleLabel.leadingAnchor.constraint(equalTo: iconImage.trailingAnchor, constant: 10),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+            
+            subtitleLabel.centerYAnchor.constraint(equalTo: iconImage.centerYAnchor, constant: 15),
+            subtitleLabel.leadingAnchor.constraint(equalTo: iconImage.trailingAnchor, constant: 10),
+            subtitleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ ORI/View/Navigaion/NotificationsViewController.swift
@@ -0,0 +1,83 @@
+//
+//  NotificationsViewController.swift
+//  ORI
+//
+//  Created by Song Kim on 11/6/24.
+//
+
+import UIKit
+
+class NotificationsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let viewModel = NotificationViewModel()
+    
+    private let noPostsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "받은 알람이 없습니다."
+        label.textColor = .gray
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.isHidden = true
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        modalNavigationBar(text: "// AL")
+        
+        setupTableView()
+        setupRefreshControl()
+        view.addSubview(noPostsLabel)
+        
+        NSLayoutConstraint.activate([
+            noPostsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            noPostsLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        loadDataAndUpdateUI()
+    }
+    
+    func setupRefreshControl() {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshData), for: .valueChanged)
+        viewModel.tableView.refreshControl = refreshControl
+    }
+    
+    @objc func refreshData() {
+        loadDataAndUpdateUI()
+    }
+    
+    func setupTableView() {
+        viewModel.tableView.frame = view.bounds
+        viewModel.tableView.dataSource = self
+        viewModel.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "TILPostCell")
+        view.addSubview(viewModel.tableView)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.lists.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = NotificationCell()
+        cell.subtitleLabel.text = viewModel.lists[indexPath.row].content
+        cell.titleLabel.text = "REVIEW AL \(viewModel.lists[indexPath.row].executeTime.prefix(10).description)"
+        return cell
+    }
+    
+    private func updateNoPostsLabelVisibility() {
+        noPostsLabel.isHidden = !viewModel.lists.isEmpty
+    }
+    
+    private func loadDataAndUpdateUI() {
+        viewModel.loadNotiList() { [weak self] in
+            DispatchQueue.main.async {
+                self?.updateNoPostsLabelVisibility()
+                self?.viewModel.tableView.reloadData()
+                self?.viewModel.tableView.refreshControl?.endRefreshing()
+            }
+        }
+    }
+}
