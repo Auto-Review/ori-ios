59f9421f3ffd435dab32a2cae23f1620cc2813c4
ksiomng <nadana0929@gmail.com>
2025-04-01 16:35:26 +0900

    Fix: 체크리스트가 늘어나면 스크롤이 가능해야해서 코드 수정 !

--- ORI/View/Main/MainViewController.swift
+++ ORI/View/Main/MainViewController.swift
@@ -11,7 +11,23 @@ import FSCalendar
 class MainViewController: UIViewController, FSCalendarDelegate, FSCalendarDelegateAppearance {
     let viewModel = MainViewModel()
     
-    let imageView = UIImageView(image: UIImage(named: "mainimage"))
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    private let contentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "mainimage"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
     
     private let calendarView: FSCalendar = {
         let calendar = FSCalendar()
@@ -29,26 +45,10 @@ class MainViewController: UIViewController, FSCalendarDelegate, FSCalendarDelega
         return calendar
     }()
     
-    private let prevButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("〈", for: .normal)
-        button.setTitleColor(.gray, for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-    
-    private let nextButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("〉", for: .normal)
-        button.setTitleColor(.gray, for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-    
     lazy var mainpageTextLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "// CHEAK LIST"
+        label.text = "// CHECK LIST"
         label.font = UIFont.blackhansans(ofSize: 20)
         return label
     }()
@@ -73,62 +73,57 @@ class MainViewController: UIViewController, FSCalendarDelegate, FSCalendarDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        mainNavigationBar()
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        
+        contentView.addSubview(imageView)
+        contentView.addSubview(calendarView)
+        contentView.addSubview(mainpageTextLabel)
+        contentView.addSubview(grayBackgroundView)
+        contentView.addSubview(todayListLabel)
+        
+        setupConstraints()
         loadDataAndUpdateUI()
-        setupRefreshControl()
-
-        view.addSubview(imageView)
-        view.addSubview(calendarView)
-        view.addSubview(mainpageTextLabel)
-        view.addSubview(grayBackgroundView)
-        view.addSubview(prevButton)
-        view.addSubview(nextButton)
-        view.addSubview(todayListLabel)
-        
-        calendarView.delegate = self
-        
-        prevButton.addTarget(self, action: #selector(prevMonth), for: .touchUpInside)
-        nextButton.addTarget(self, action: #selector(nextMonth), for: .touchUpInside)
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        
+    }
+    
+    private func setupConstraints() {
         NSLayoutConstraint.activate([
-            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
             imageView.heightAnchor.constraint(equalToConstant: 90),
             
-            calendarView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
-            calendarView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
             calendarView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
+            calendarView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 40),
+            calendarView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -40),
             calendarView.heightAnchor.constraint(equalToConstant: 330),
             
-            prevButton.centerYAnchor.constraint(equalTo: calendarView.topAnchor, constant: 25),
-            prevButton.leadingAnchor.constraint(equalTo: calendarView.leadingAnchor),
-            
-            nextButton.centerYAnchor.constraint(equalTo: calendarView.topAnchor, constant: 25),
-            nextButton.trailingAnchor.constraint(equalTo: calendarView.trailingAnchor),
-            
             mainpageTextLabel.topAnchor.constraint(equalTo: calendarView.bottomAnchor, constant: 30),
-            mainpageTextLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 18),
+            mainpageTextLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 18),
             
             grayBackgroundView.topAnchor.constraint(equalTo: mainpageTextLabel.bottomAnchor, constant: 14),
-            grayBackgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 18),
-            grayBackgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -18),
-            grayBackgroundView.heightAnchor.constraint(equalToConstant: 130),
+            grayBackgroundView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 18),
+            grayBackgroundView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -18),
+            grayBackgroundView.heightAnchor.constraint(equalToConstant: 500),
             
             todayListLabel.topAnchor.constraint(equalTo: grayBackgroundView.topAnchor, constant: 10),
             todayListLabel.leadingAnchor.constraint(equalTo: grayBackgroundView.leadingAnchor, constant: 10),
-            todayListLabel.trailingAnchor.constraint(equalTo: grayBackgroundView.trailingAnchor, constant: -10)
+            todayListLabel.trailingAnchor.constraint(equalTo: grayBackgroundView.trailingAnchor, constant: -10),
+            todayListLabel.bottomAnchor.constraint(equalTo: grayBackgroundView.bottomAnchor, constant: -10),
+            
+            grayBackgroundView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -20)
         ])
-    }
-    
-    func setupRefreshControl() {
-        let refreshControl = UIRefreshControl()
-        refreshControl.addTarget(self, action: #selector(refreshData), for: .valueChanged)
-    }
-    
-    @objc func refreshData() {
-        loadDataAndUpdateUI()
     }
     
     private func loadDataAndUpdateUI() {
@@ -136,7 +131,7 @@ class MainViewController: UIViewController, FSCalendarDelegate, FSCalendarDelega
         
         viewModel.loadNotiList { [weak self] in
             guard let self = self else { return }
-            DispatchQueue.main.async{
+            DispatchQueue.main.async {
                 self.calendarView.select(today)
                 self.calendarView.reloadData()
                 
@@ -146,22 +141,9 @@ class MainViewController: UIViewController, FSCalendarDelegate, FSCalendarDelega
         }
     }
     
-    // todayList 업데이트
     func updateTodayList() {
         let todayDates = viewModel.todayList.joined(separator: "\n")
         todayListLabel.text = todayDates
-    }
-    
-    @objc private func prevMonth() {
-        let currentPage = calendarView.currentPage
-        let previousMonth = Calendar.current.date(byAdding: .month, value: -1, to: currentPage)!
-        calendarView.setCurrentPage(previousMonth, animated: true)
-    }
-    
-    @objc private func nextMonth() {
-        let currentPage = calendarView.currentPage
-        let nextMonth = Calendar.current.date(byAdding: .month, value: 1, to: currentPage)!
-        calendarView.setCurrentPage(nextMonth, animated: true)
     }
 }
 
@@ -170,7 +152,7 @@ extension MainViewController {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
         let dateString = dateFormatter.string(from: date)
-
+        
         if viewModel.highlightedDates.contains(dateString) {
             return .baseYellow
         }
