bbd48ffa4fa58386843e3a139638ffa2e5bda053
ksiomng <nadana0929@gmail.com>
2025-04-14 15:53:09 +0900

    Fix: 데이터 포맷형식을 자주 사용할 꺼 같아서 따로 생성

---
+++ ORI/Utils/DateFormat.swift
@@ -0,0 +1,26 @@
+//
+//  DateFormat.swift
+//  ORI
+//
+//  Created by Song Kim on 4/14/25.
+//
+
+import Foundation
+
+class DateFormat {
+    static func onlyDay(date: Date) -> String{
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let dateString = dateFormatter.string(from: date)
+        return dateString
+    }
+    
+    static func dayTime(str: String) -> String {
+        let components = str.split(separator: "T")
+        if components.count > 1 {
+            let formattedString = components[0] + " " + components[1].prefix(8)
+            return String(formattedString)
+        }
+        return ""
+    }
+}
