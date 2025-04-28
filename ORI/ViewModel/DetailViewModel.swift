f7ed3b27c63f2bc6da329e3f4991024bd23117de
ksiomng <nadana0929@gmail.com>
2025-04-28 17:37:39 +0900

    Feat: 포스트 댓글리스트 가져오기

--- ORI/ViewModel/DetailViewModel.swift
+++ ORI/ViewModel/DetailViewModel.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 class DetailViewModel {
     var myInfo: Member = Member(id: 0, email: "", nickname: "")
+    var postComments: Comments = Comments(commentList: [], totalPage: 0)
     
     init() {
         fetchMyData()
@@ -19,6 +20,17 @@ class DetailViewModel {
             switch result {
             case .success(let posts):
                 self?.myInfo = posts
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func fetchCommentList(tilPostId: Int, page: Int, size: Int) {
+        fetchTILCommentList(tilPostId: tilPostId, page: page, size: size) { [weak self] result in
+            switch result {
+            case .success(let posts):
+                self?.postComments = posts
             case .failure(let error):
                 print(error.localizedDescription)
             }
