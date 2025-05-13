13834b73441bfb72aee8cf42ee8ec36f04766854
ksiomng <nadana0929@gmail.com>
2025-05-13 10:33:57 +0900

    Feat: CodeDetail 데이터 받아오기

--- ORI/Data/DTO/Code.swift
+++ ORI/Data/DTO/Code.swift
@@ -52,3 +52,26 @@ struct Code: Decodable {
         case isPublic = "public"  // 매핑 처리
     }
 }
+
+struct CodeDetail: Decodable {
+    let id: Int
+    let writerId: Int
+    let writerEmail: String
+    let writerNickName: String
+    let title: String
+    let level: Int
+    let reviewDay: String
+    let description: String
+    let language: String
+    let code: String
+    let dtoList: [Review]
+    let createDate: String
+}
+
+struct Review: Decodable {
+    let id: Int
+    let description: String
+    let code: String
+    let createdAt: String
+    let updatedAt: String
+}
